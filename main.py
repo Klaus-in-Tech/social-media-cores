0028b904139bf1ffad3ee5b1c344843b1fdb4ba3
Klaus-in-Tech <kkallan07@gmail.com>
2024-09-19 15:27:08 +0300

    retrieving data passed to the API

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.params import Body
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your data"}
+
+@app.post("/createpost")
+def createpost(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title: {payload['title']} content: {payload['content']}"}
